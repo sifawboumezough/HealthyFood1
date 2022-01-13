9ff12a5d1e84faa866e28ea61a3a610a6bb37f1f
Elmahdi <gliouinemahdi0@gmail.com>
2022-01-13 18:51:43 +0100

    Version5

---
+++ html&css/Product.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/product.css">
+    <!-- font awesome 5CDN -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <title>Product</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="sidebar">
+            <div class="logo"> 
+            <img src="./images/Logo.jpg" alt="">
+            </div>
+            <ul>
+                <li>
+                    <a href="#" class="active">
+                        <span class="icon"><i class="fas fa-shopping-basket"></i>
+                        <span class="item">Products</span>
+                    </a>
+                </li>    
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fas fa-plus-circle"></i>    
+                        <span class="item">Add Product</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="logout" class="icon"><i class="fas fa-sign-out-alt"></i>
+                        <span class="item">Log Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="section">
+            <div class="top_navbar">
+                    <h3>Products</h3>
+                    <div class="email">
+                        <span class="icon"><i class="far fa-user-circle"></i>
+                        <span class="item"><h4> Robert Dinero</h4>
+                        <h5>Robertdeniro@gmail.com</h5></span>
+                    </div>
+             </div>
+        </div>
+
+     </div>
+    
+   
+    <script>
+
+
+
+
+
+
+
+
+    </script>
+</body>
+</html>
