812421af8a9b547c955cf1a9da0ad0223e148910
Sif4w <bmz.sifaw1@gmail.com>
2022-01-19 15:18:46 +0100

    vers

--- html&css/Product.php
+++ html&css/Product.php
@@ -177,6 +177,7 @@
         var hamburger = document.querySelector(".hamburger");
         hamburger.addEventListener("click", function(){
         document.querySelector("body").classList.toggle("active");
+
     })
     </script>
    
